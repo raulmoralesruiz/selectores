641ce410dc015561470628c471ac6ccc20294c19
Raúl Morales <raulmdaw@gmail.com>
2024-01-28 00:52:15 +0100

    268. Obtener países por region

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,20 +1,29 @@
 import { Injectable } from '@angular/core';
 import { Region, SmallCountry } from '../interfaces/country.interface';
+import { Observable, of, tap } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CountriesService {
+  private baseUrl: string = 'https://restcountries.com/v3.1';
   private _regions: Region[] = [ Region.Africa, Region.Americas, Region.Asia, Region.Europe, Region.Oceania];
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
 
   get regions(): Region[] {
     return [ ...this._regions ];
   }
 
-  getCountriesByRegion( region: Region ): SmallCountry[] {
+  getCountriesByRegion( region: Region ): Observable<SmallCountry[]> {
+    if (!region) return of([]);
+    const url: string = `${this.baseUrl}/region/${region}?fields=cca3,name,borders`;
 
-    return [];
+    return this.http.get<SmallCountry[]>(url).pipe(
+      tap( res => console.log(res))
+    );
   }
 }
